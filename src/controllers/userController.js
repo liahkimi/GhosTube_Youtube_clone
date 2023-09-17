e4ecb3997a8243b53fe9ba30d56229db68f86a67
Gaeun Kim <maruanna1994@gmail.com>
2023-09-17 09:16:48 +0900

    wetube-lecture-7.17~20

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -62,8 +62,78 @@ export const postLogin = async (req,res)=>{
     req.session.user = user;
     return res.redirect("/");
 };
+export const startGithubLogin = (req,res) => {
+    const baseUrl="https://github.com/login/oauth/authorize";
+    const config = {
+        client_id: process.env.GH_CLIENT,
+        allow_signup: false,
+        scope: "read:user user:email",
+    }
+    const params =  new URLSearchParams(config).toString();
+    const finalUrl=`${baseUrl}?${params}`;
+    return res.redirect(finalUrl);
+}
+export const finishGithubLogin = async(req,res) => {
+    const baseUrl="https://github.com/login/oauth/access_token"
+    const config ={
+        client_id: process.env.GH_CLIENT,
+        client_secret : process.env.GH_SECRET,
+        code: req.query.code,
+    }
+   const params = new URLSearchParams(config).toString();
+   const finalUrl = `${baseUrl}?${params}`;
+
+
+   const tokenRequest = await( 
+    await fetch(finalUrl, { //fetch로 데이터 받아옴
+        method: "POST",//url에 POST request 보내기
+        headers:{
+            Accept: "application/json",
+        },
+    })
+).json();
+   if("access_token" in tokenRequest){
+    const {access_token} = tokenRequest;
+    const apiUrl ="https://api.github.com";
+    const userData = await(
+        await fetch(`${apiUrl}/user`, {
+            headers:{
+                Authorization: `token ${access_token}`,
+            },
+        })  
+    ).json();
+    console.log(userData);
+    const emailData = await(
+        await fetch(`${apiUrl}/user/emails`, {
+            headers:{
+                Authorization: `token ${access_token}`
+                },
+            })
+        ).json();
+        console.log(emailData);
+        //깃헙이 주는 list에서 primary이면서 verified된 email객체를 찾기
+        const emailObj = emailData.find(
+            (email) => email.primary === true && email.verified === true
+        );
+        if(!emailObj){
+            return res.redirect("/login")
+        }
+        //같은 email를 가진 user가 이미 있다면, 그 유저를 로그인 시켜주기
+        const existingUser = await User.findOne({email: emailObj.email});
+        if(existingUser){
+            req.session.loggedIn = true;
+            req.session.user = existingUser;
+            return res.redirect("/")
+        }else{
+            //같은 email를 가진 user가 없다면, 계정 생성하기 추가
+        }
+        }else{
+            return res.redirect("/login")
+        };
+
+
+}
 export const edit = (req,res) => res.send("Edit User")
 export const remove = (req,res) => res.send("Remove User")
 export const logout = (req,res) => res.send("Logout")
 export const see = (req,res) => res.send("See User")
-
