b2b562eb529c8644e498d6fd670d45a2da7b47a6
Gaeun Kim <maruanna1994@gmail.com>
2023-09-30 17:41:13 +0900

    change password

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000; //클라우드타입에선 4000으로 포트 지정해주면 됌
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
